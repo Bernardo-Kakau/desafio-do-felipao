9dfaf1042fccfe0a539ba968a36db5a3ecce331b
bernardo <bernardoricardokakau@gmail.com>
2024-10-22 19:18:59 -0300

    teste

---
+++ D001/index.js
@@ -0,0 +1,29 @@
+let heroi = "luffy"
+let Xp = 1000
+
+switch (Xp) {
+    case 0 < 1000:
+        console.log("O heroi de nome  esta no nivel ferro")
+        break
+    case 1001 > 2000:
+        console.log("Bronze") 
+        break
+    case 2001 > 5000:
+        console.log("prata")
+        break
+    case 5001 > 7000:
+        console.log("Ouro")
+        break
+    case 7001 > 8000:
+        console.log("Platina")
+        break
+    case 8001 > 9000:
+        console.log("ascendente")
+        break
+    case 9001 > 10000:
+        console.log("imortal")
+        break
+    case 10000 > 10001:
+        console.log("radiante")
+        break
+}
